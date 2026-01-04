7f85b107561d96ed30047750dea1e8c4f3601675
mdjameel04 <jameeltony67@gmail.com>
2026-01-04 21:03:31 +0530

    inital commit

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,17 +1,21 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { UserDetailContext } from '@/context/UserDetailContext';
 const Provider = ({children}:any) => {
-
+const [userDetail, setUserDetail] = useState();
     useEffect(()=>{
         CreateNewUser()
     },[])
     const CreateNewUser = async()=>{
         const result = await axios.post('/api/user',{})
         console.log(result.data)
+        setUserDetail(result?.data)
     }
   return (
+<UserDetailContext.Provider value={{userDetail, setUserDetail}}>
     <div>{children}</div>
+    </UserDetailContext.Provider>
   )
 }
 
